8b465d6fd3b72010679c0c4441432ea88797a121
unknown <sarjac@ktu.edu>
2024-04-11 22:20:46 +0300

    Add images to all recommendation buttons

--- app/screens/RekomendacijosScreen.tsx
+++ app/screens/RekomendacijosScreen.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { Image, ImageStyle, TextStyle, ViewStyle, View, Pressable, ImageProps } from "react-native"
 import { AppStackScreenProps } from "app/navigators"
@@ -6,30 +6,44 @@ import { Button, Card, Screen, Text } from "app/components"
 import { colors, spacing } from "../theme"
 
 //Image imports
-const Sitting = require("../../assets/images/Rekomendacijos/Sitting.png"); 
+const Sitting = require("../../assets/images/Rekomendacijos/Sitting.png");
+const Standing = require("../../assets/images/Rekomendacijos/Standing.png"); 
+const Running = require("../../assets/images/Rekomendacijos/Running.png"); 
+const Lying = require("../../assets/images/Rekomendacijos/Lying.png"); 
+const Walking = require("../../assets/images/Rekomendacijos/Walking.png"); 
 
 interface RekomendacijosScreenProps extends AppStackScreenProps<"Rekomendacijos"> {}
 
-// Styled card component that wraps the Card component and preapplies default values/styles
-const StyledCard: FC<{ label: string, image?: ImageProps }> = ({ label, image }) => (
-  <View style={{ width: '45%', margin: spacing.xs }}>
-    <Button preset="default" style={$buttonCardStyle}>
-      <Image source={image} style={$imageStyle}></Image>
-      <Text style={$buttonLabel} preset="formLabel">{label}</Text>
-    </Button>
-  </View>
-);
+const StyledCard: FC<{ label: string, image?: ImageProps }> = ({ label, image }) => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handleClick = () => {
+    console.log("Button clicked");
+  }
+
+  return (
+  <Pressable style={() => [
+    $buttonCardStyle,
+    {
+      backgroundColor: isPressed ? colors.palette.neutral300 : colors.palette.neutral100,
+    },
+    ]} onPressIn={() => {setIsPressed(true)}} onPressOut={() => {setIsPressed(false)}} onPress={handleClick}>
+    <Image source={image} style={$imageStyle}></Image>
+    <Text style={$buttonLabel} preset="formLabel">{label}</Text>
+  </Pressable>
+  );
+};
 
 export const RekomendacijosScreen: FC<RekomendacijosScreenProps> = observer(function RekomendacijosScreen() {
   return (
-    <Screen preset="scroll" safeAreaEdges={["top"]} contentContainerStyle={$container}>
+    <Screen safeAreaEdges={["top"]} contentContainerStyle={$container}>
       <Text style={$title} preset="heading" text="Rekomendacijos" />
       <View style={$cardsContainer}>
         <StyledCard label="Sėdėjimas" image={Sitting}></StyledCard>
-        <StyledCard label="Vaikščiojimas"></StyledCard>
-        <StyledCard label="Stovėjimas"></StyledCard>
-        <StyledCard label="Bėgimas"></StyledCard>
-        <StyledCard label="Gulėjimas"></StyledCard>
+        <StyledCard label="Vaikščiojimas" image={Walking}></StyledCard>
+        <StyledCard label="Stovėjimas" image={Standing}></StyledCard>
+        <StyledCard label="Bėgimas" image={Running}></StyledCard>
+        <StyledCard label="Gulėjimas" image={Lying}></StyledCard>
       </View>
     </Screen>
   )
@@ -48,23 +62,28 @@ const $cardsContainer: ViewStyle = {
   flexDirection: "row",
   flexWrap: "wrap",
   justifyContent: "space-between",
-  alignItems: "flex-start"
+  alignItems: "flex-start",
 }
 
 const $buttonCardStyle: ViewStyle = {
-  backgroundColor: colors.palette.neutral100,
+  width: '45%',
   elevation: 4,
   padding: null,
   borderRadius: 8,
   display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
   flexDirection: "column",
+  margin: spacing.xs
 }
 
 const $imageStyle: ImageStyle = {
   resizeMode: 'contain',
-  height: 100,
-  width: 100,
+  maxHeight: 80,
+  width: 80,
+  marginVertical: spacing.sm
 }
 
 const $buttonLabel: TextStyle = {
+  marginBottom: spacing.xs
 }
