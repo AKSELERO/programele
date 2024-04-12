cb10ffd914243ef6995973eeea0dcc2f8253a066
unknown <sarjac@ktu.edu>
2024-04-12 07:07:52 +0300

    Add faux data for Tikslai screen

--- app/screens/TikslaiScreen.tsx
+++ app/screens/TikslaiScreen.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle, TextStyle } from "react-native"
 import { AppStackScreenProps } from "app/navigators"
@@ -9,15 +9,68 @@ import { colors, spacing } from "../theme"
 
 interface TikslaiScreenProps extends AppStackScreenProps<"Tikslai"> {}
 
-export const TikslaiScreen: FC<TikslaiScreenProps> = observer(function TikslaiScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+interface Goal {
+  name: string
+  enabled: boolean
+  timeframe?: number // Hours
+  moreThan?: boolean // Is the goal less than or more than specified hours
+  goalHours?: number // Goal, specified in hours
+}
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+const initGoals = [
+  {
+    name: "Vaikščiojimas",
+    enabled: false,
+    timeFrame: 24,
+    moreThan: true,
+    goalHours: 2
+  },
+  {
+    name: "Bėgiojimas",
+    enabled: false,
+    timeFrame: 24,
+    moreThan: true,
+    goalHours: 2
+  },
+  {
+    name: "Sėdėjimas",
+    enabled: false,
+    timeFrame: 24,
+    moreThan: true,
+    goalHours: 2
+  },
+  {
+    name: "Stovėjimas",
+    enabled: false,
+    timeFrame: 24,
+    moreThan: true,
+    goalHours: 2
+  },
+  {
+    name: "Gulėjimas",
+    enabled: false,
+    timeFrame: 24,
+    moreThan: true,
+    goalHours: 2
+  },
+]
+
+export const TikslaiScreen: FC<TikslaiScreenProps> = observer(function TikslaiScreen() {
+  const [goalData, setGoalData] = useState<Goal[]>(initGoals)
+
+  const renderGoal = (goal: Goal) => {
+    return (
+      <Text key={goal.name}>{goal.name}</Text>
+    )
+  }
+
   return (
     <Screen preset="scroll" safeAreaEdges={["top"]} contentContainerStyle={$container}>
       <Text style={$title} preset="heading" text="Tikslų redagavimas" />
+
+      {goalData.map((goal) => {
+        return renderGoal(goal);
+      })}
     </Screen>
   )
 })
