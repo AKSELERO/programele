d574b31a145f7f1ccf0d9f6debda33cb01f92471
unknown <sarjac@ktu.edu>
2024-04-17 17:53:10 +0300

    Add date range state

--- app/screens/StatistikaScreen.tsx
+++ app/screens/StatistikaScreen.tsx
@@ -1,23 +1,36 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle, TextStyle } from "react-native"
 import { AppStackScreenProps } from "app/navigators"
 import { Screen, Text } from "app/components"
 import { colors, spacing } from "../theme"
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "app/models"
 
-interface StatistikaScreenProps extends AppStackScreenProps<"Statistika"> {}
+interface StatistikaScreenProps extends AppStackScreenProps<"Statistika"> { }
+
+const formatDate = (date: Date) => {
+  return date.toISOString().split('T')[0];
+};
 
 export const StatistikaScreen: FC<StatistikaScreenProps> = observer(function StatistikaScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  // Get the current date
+  const currentDate = new Date();
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+  // Get the date for 7 days ago
+  const sevenDaysAgo = new Date(currentDate);
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+  const [statisticsDateRange, setStatisticsDateRange] = useState({
+    start: formatDate(sevenDaysAgo),
+    end: formatDate(currentDate),
+  });
+
+
+
+
   return (
     <Screen preset="scroll" safeAreaEdges={["top"]} contentContainerStyle={$container}>
       <Text style={$title} preset="heading" text="Statistika" />
+      <Text>{statisticsDateRange.start} - {statisticsDateRange.end}</Text>
     </Screen>
   )
 })
