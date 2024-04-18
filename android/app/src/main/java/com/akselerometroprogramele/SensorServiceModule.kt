403b6dd2be3db75fe3be7361f3957c5dac50d599
aleksas753 <aleksas.juzukonis@ktu.edu>
2024-04-18 17:51:05 +0300

    permission request while running in background

--- android/app/src/main/java/com/akselerometroprogramele/SensorServiceModule.kt
+++ android/app/src/main/java/com/akselerometroprogramele/SensorServiceModule.kt
@@ -4,6 +4,12 @@ import android.content.Intent
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import com.facebook.react.bridge.*
+import androidx.core.app.ActivityCompat
+import android.os.Build
 
 class SensorServiceModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -12,7 +18,25 @@ class SensorServiceModule(reactContext: ReactApplicationContext) : ReactContextB
     }
 
     @ReactMethod
-    fun startService() {
+    fun startService(promise: Promise) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(reactApplicationContext, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                startSensorService()
+                promise.resolve(null) // Resolve promise when service starts successfully
+            } else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(currentActivity!!, Manifest.permission.POST_NOTIFICATIONS)) {
+                    promise.reject("PERMISSION_DENIED_NEEDS_RATIONALE", "Permission denied. Needs rationale.")
+                } else {
+                    promise.reject("PERMISSION_DENIED", "Permission denied without rationale.")
+                }
+            }
+        } else {
+            startSensorService()
+            promise.resolve(null) // Resolve promise when service starts successfully on older Android versions
+        }
+    }
+
+    private fun startSensorService() {
         val serviceIntent = Intent(reactApplicationContext, SensorService::class.java)
         reactApplicationContext.startForegroundService(serviceIntent)
     }
@@ -23,3 +47,4 @@ class SensorServiceModule(reactContext: ReactApplicationContext) : ReactContextB
         reactApplicationContext.stopService(serviceIntent)
     }
 }
+
