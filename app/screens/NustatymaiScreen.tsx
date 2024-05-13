8a13c3352c33260acb057da52522a399e0bc51e3
unknown <sarjac@ktu.edu>
2024-05-13 19:11:22 +0300

    Add init settings

--- app/screens/NustatymaiScreen.tsx
+++ app/screens/NustatymaiScreen.tsx
@@ -6,15 +6,54 @@ import { AppStackScreenProps } from "app/navigators"
 import { Screen, Text, Toggle, ToggleProps } from "../components"
 import { colors, spacing } from "../theme"
 
-interface NustatymaiScreenProps extends AppStackScreenProps<"Nustatymai"> {}
+interface NustatymaiScreenProps extends AppStackScreenProps<"Nustatymai"> { }
 
 // Toggle button
 function ControlledToggle({ value, onChange, ...props }: ToggleProps & { value: boolean; onChange: (newValue: boolean) => void }) {
   return <Toggle {...props} value={value} onPress={() => onChange(!value)} />;
 }
 
+interface Setting {
+  name: string,
+  description?: string,
+  isTurnedOn: false
+}
+
+const initSettings = [
+  // {
+  //   name: "",
+  //   description: "",
+  //   isTurnedOn: false
+  // },
+  {
+    name: "Pranešimų siuntimas",
+    description: "Ar programėlė turėtų siųsti pranešimus? (Notifications)",
+    isTurnedOn: true
+  },
+  {
+    name: "Trumpos statistikos pranešimai",
+    description: "Ar programėlė turėtų siųsti pranešimus su trumpa statistikos apžvalga?",
+    isTurnedOn: true
+  },
+  {
+    name: "Nepasiekto tikslo priminimai",
+    description: "Ar programėlė turėtų priminti apie dar nepasiektus dienos tikslus?",
+    isTurnedOn: true
+  },
+  {
+    name: "Pasiekto tikslo sveikinimai",
+    description: "Ar programėlė turėtų pranešti apie pasiektą dienos tikslą?",
+    isTurnedOn: true
+  },
+  {
+    name: "Ilgo sėdėjimo perspėjimas",
+    description: "Ar programėlė turėtų perspėti apie aptiktą ilgą nenutrauktą sėdėjimą?",
+    isTurnedOn: true
+  },
+]
+
 export const NustatymaiScreen: FC<NustatymaiScreenProps> = observer(function NustatymaiScreen() {
-// Centralized state for all toggles
+  // Centralized state for all toggles
   const [settings, setSettings] = useState({
     setting1: false,
     setting2: false,
@@ -30,14 +69,14 @@ export const NustatymaiScreen: FC<NustatymaiScreenProps> = observer(function Nus
   };
 
   // Styling variables
-  let toggleColorBgOff = colors.palette.neutral600;
+  const toggleColorBgOff = colors.palette.neutral600;
 
   return (
     <Screen preset="scroll" safeAreaEdges={["top"]} contentContainerStyle={$container}>
       <Text style={$title} preset="heading" text="Nustatymai" />
       <View style={$optionsContainer}>
-      {/* Kol kas tiesiog sugeneruojam 6 default nustatymo blokus, kai turėsim actual nustatymus reiks juos sudėt manually */}
-      {Object.entries(settings).map(([key, value]) => (
+        {/* Kol kas tiesiog sugeneruojam 6 default nustatymo blokus, kai turėsim actual nustatymus reiks juos sudėt manually */}
+        {Object.entries(settings).map(([key, value]) => (
           <ControlledToggle
             key={key}
             variant="switch"
@@ -46,7 +85,7 @@ export const NustatymaiScreen: FC<NustatymaiScreenProps> = observer(function Nus
             inputOuterStyle={{ backgroundColor: toggleColorBgOff }}
             labelPosition="left"
             LabelTextProps={{ size: "md" }}
-            containerStyle={{ backgroundColor: colors.palette.neutral100, elevation: 4, padding: spacing.md, borderRadius: 8}}
+            containerStyle={{ backgroundColor: colors.palette.neutral100, elevation: 4, padding: spacing.md, borderRadius: 8 }}
             value={value}
             onChange={() => handleToggle(key as keyof typeof settings)}
           />
