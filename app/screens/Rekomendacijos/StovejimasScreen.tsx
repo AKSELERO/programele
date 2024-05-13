bd20c5c58f3c463cb5f103eb97d89959356c9f3c
unknown <sarjac@ktu.edu>
2024-05-13 18:54:37 +0300

    Add standing reccomendations

--- app/screens/Rekomendacijos/StovejimasScreen.tsx
+++ app/screens/Rekomendacijos/StovejimasScreen.tsx
@@ -17,66 +17,22 @@ export const StovejimasScreen: FC<StovejimasScreenProps> = observer(function Sto
 
         <View style={$reccomendationSection}>
           <Text preset="subheading" text="Trumpai"></Text>
-          <Text text="Vidutiniškai žmogus per dieną sėdi 8 valandas. Kad būtų išvengtos sėdėjimo keliamos sveikatos problemos, rekomenduojama bent 150 minučių per savaitę užsiimti vidutinio intensyvumo sportu."></Text>
+          <Text text="Per dieną rekomenduojama stovėti bent 2 valandas, optimalus laikas - 4 valandos."></Text>
         </View>
 
-        <View style={$bottomBorder}>
-
-        </View>
-
-        <View style={$imageContainer}>
-          <Image source={require("../../../assets/images/Rekomendacijos/sittingPosture.png")} style={$image}></Image>
-          <Text text="Taisyklingas sėdėjimas"></Text>
-        </View>
+        <View style={$bottomBorder}></View>
 
         <View style={$reccomendationSection}>
           <Text preset="subheading" text="Patarimai"></Text>
-          <Text text="Išvengti ilgo sėdėjimo darbovietėse galite šių rekomendacijų pagalba:"></Text>
+          <Text text="Ilgą laiką praleisti stovint gali būti nepatogu, o kartais net ir skausminga. Prailginti savo vidutinį dienos stovėjimo laiką galite šių patarimų pagalba:"></Text>
 
           <View style={$bulletListContainer}>
-            <Text text={`\u2022 naudoti tiek sėdimus, tiek stovimus darbastalius`}></Text>
-            <Text text={`\u2022 kas 30 minučių keisti padėtį iš sėdimos į stovimą`}></Text>
-            <Text text={`\u2022 rengti darbo susitikimus vaikštant`}></Text>
-            <Text text={`\u2022 reguliariai pratempti raumenis`}></Text>
-            <Text text={`\u2022 nueiti pokalbėti su kolega gyvai vietoj elektroninio laiško siuntimo`}></Text>
-          </View>
-        </View>
+            <Text text={`\u2022 jei ilgai stovima, rekomenduojama kas 30 minučių pratempti raumenis`}></Text>
+            <Text text={`\u2022 stovint rekomenduojama išlaikyti taisyklingą laikyseną, stovėti ant abiejų kojų`}></Text>
+            <Text text={`\u2022 įsigyti stovėjimo stalą`}></Text>
+            <Text text={`\u2022 kalbant telefonu išeiti pasivaikščioti`}></Text>
+            <Text text={`\u2022 automobilį palikti toliau nuo darbovietės, kad reiktų iki jos paeiti`}></Text>
 
-        <View style={$reccomendationSection}>
-          <Text preset="subheading" text="Sėdėjimo rizika"></Text>
-          <Text text="Ilgas laikas praleistas sėdint kelia riziką išsivystyti sveikatos problemoms."></Text>
-          <View style={$bulletListContainer}>
-            <Text text={`\u2022 Maža rizika - <4 valandos per parą`}></Text>
-            <Text text={`\u2022 Vidutinė rizika - 4-8 valandos per parą`}></Text>
-            <Text text={`\u2022 Didelė rizika - 8-11 valandų per parą`}></Text>
-            <Text text={`\u2022 Labai didelė rizika - daugiau nei 11 valandų per parą`}></Text>
-          </View>
-        </View>
-
-        <View style={$reccomendationSection}>
-          <Text preset="subheading" text="Ilgo sėdėjimo diskomfortas"></Text>
-          <Text text="Ilgą laiką praleidžiant vienoje pozicijoje, tam tikrose kūno dalyse gali atsirasti diskomfortas, skausmas. Diskomfortas dėl per ilgo sėdėjimo gali būti jaučiamas šiose srityse:"></Text>
-          <View style={$bulletListContainer}>
-            <Text text={`\u2022 kaklas`}></Text>
-            <Text text={`\u2022 pečiai`}></Text>
-            <Text text={`\u2022 kojos`}></Text>
-            <Text text={`\u2022 apatinė nugaros dalis`}></Text>
-            <Text text={`\u2022 šlaunys`}></Text>
-            <Text text={`\u2022 rankos ir riešai`}></Text>
-          </View>
-        </View>
-
-        <View style={$reccomendationSection}>
-          <Text preset="subheading" text="Sveikatos problemos, kurias gali sukelti per ilgas sėdėjimas"></Text>
-          <View style={$bulletListContainer}>
-            <Text text={`\u2022 širdies ligos`}></Text>
-            <Text text={`\u2022 viršsvoris`}></Text>
-            <Text text={`\u2022 vėžys`}></Text>
-            <Text text={`\u2022 miego sutrikimai`}></Text>
-            <Text text={`\u2022 demencija`}></Text>
-            <Text text={`\u2022 kraujagyslių ligos (venų varikozė)`}></Text>
-            <Text text={`\u2022 nerimas`}></Text>
-            <Text text={`\u2022 nugaros problemos`}></Text>
           </View>
         </View>
 
